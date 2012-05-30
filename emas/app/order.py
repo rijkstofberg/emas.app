57d23ed122555e63335d3ff9f24ecb205be48883
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-05-31 00:08:49 +0200

    Remove boiler plate.

--- emas/app/order.py
+++ emas/app/order.py
@@ -21,42 +21,15 @@ from plone.formwidget.contenttree import ObjPathSourceBinder
 from emas.app import MessageFactory as _
 
 
-# Interface class; used to define content-type schema.
-
 class IOrder(form.Schema, IImageScaleTraversable):
     """
     Container for orderable items like products and services
     """
-    
-    # If you want a schema-defined interface, delete the form.model
-    # line below and delete the matching file in the models sub-directory.
-    # If you want a model-based interface, edit
-    # models/order.xml to define the content type
-    # and add directives here as necessary.
-    
-    form.model("models/order.xml")
 
-
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
 
 class Order(dexterity.Container):
     grok.implements(IOrder)
-    
-    # Add your class methods and properties here
 
-
-# View class
-# The view will automatically use a similarly named template in
-# order_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
 
 class SampleView(grok.View):
     grok.context(IOrder)
--- emas/app/service.py
+++ emas/app/service.py
@@ -21,42 +21,15 @@ from plone.formwidget.contenttree import ObjPathSourceBinder
 from emas.app import MessageFactory as _
 
 
-# Interface class; used to define content-type schema.
-
 class IService(form.Schema, IImageScaleTraversable):
     """
     An orderable service (e.g. intelligent practice)
     """
-    
-    # If you want a schema-defined interface, delete the form.model
-    # line below and delete the matching file in the models sub-directory.
-    # If you want a model-based interface, edit
-    # models/service.xml to define the content type
-    # and add directives here as necessary.
-    
-    form.model("models/service.xml")
 
-
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
 
 class Service(dexterity.Item):
     grok.implements(IService)
     
-    # Add your class methods and properties here
-
-
-# View class
-# The view will automatically use a similarly named template in
-# service_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
 
 class SampleView(grok.View):
     grok.context(IService)
