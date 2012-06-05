cbe5f8b0c1a981f14094533d919fa37ed92ac0bf
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-06-05 21:49:58 +0200

    New views for product

--- emas/app/product.py
+++ emas/app/product.py
@@ -1,22 +1,12 @@
 from five import grok
-from plone.directives import dexterity, form
 
+from z3c.form import field
 from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
-
-from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
+from plone.directives import dexterity, form
 from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
+from plone.app.textfield.widget import RichTextWidget
+from plone.app.z3cform.wysiwyg.widget import WysiwygFieldWidget
 
 from emas.app import MessageFactory as _
 
@@ -28,12 +18,34 @@ class IProduct(form.Schema):
     price = schema.Float(
         title=_(u"Price"),
         description=_("Price in Rands."),
-        required=False,
+        required=True,
     )
+
 
 class Product(dexterity.Item):
     grok.implements(IProduct)
-    
+
+
+class AddForm(dexterity.AddForm):
+    grok.name('emas.app.product')
+
+    def updateWidgets(self):
+        self.fields['IBasic.description'].widgetFactory = WysiwygFieldWidget
+        super(AddForm, self).updateWidgets()
+
+
+class EditForm(dexterity.EditForm):
+    grok.context(IProduct)
+
+    def updateWidgets(self):
+        self.fields['IBasic.description'].widgetFactory = WysiwygFieldWidget
+        super(EditForm, self).updateWidgets()
+
+
+class View(dexterity.DisplayForm):
+    grok.context(IProduct)
+    grok.require('zope2.View')
+
 
 class SampleView(grok.View):
     grok.context(IProduct)
